d4204613568d3e22107f2719036fe3b08d463c88
Weirvict <weirvict@isu.edu>
2025-10-22 08:44:17 -0600

    pushing everything

--- Analysis/Combined_CleanUp.py
+++ Analysis/Combined_CleanUp.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, trim, upper, regexp_replace
+from pyspark.sql.functions import col, trim, upper, regexp_replace, countDistinct, length, when
 
 # Create spark session
 spark = SparkSession.builder \
@@ -18,7 +18,6 @@ df = spark.read.format("csv")\
     .option("header", "true")\
     .option("inferSchema", "true")\
     .load(f"{dir_path}/Data_For_Team2.csv")
-print(f"Beginning of rows: {df.count()}")
 #----------------------------------------------------------------------------------------------------------------------------------------------
 # Victoria's clean up code
 # Country and City clean up
@@ -28,8 +27,8 @@ df = (
       .withColumn("city", upper(trim(col("city"))))
 )
 
-# Drop rows where city or country are null
-df = df.dropna(subset=["city", "country"])
+# Drop rows where city or country are null and website
+df = df.dropna(subset=["city", "country", "ecommerce_website_name"])
 
 # Remove tabs, newlines, and extra spaces
 df = df.withColumn("city", regexp_replace("city", r"[\t\n\r]+", " "))
@@ -40,48 +39,138 @@ df = df.withColumn("city", trim(col("city")))
 valid_countries = ["CANADA", "FRANCE", "GERMANY", "GREAT BRITAIN", "MEXICO", "UNITED STATES"]
 df_clean_country = df.filter(col("country").isin(valid_countries))
 
-# Calculate Rogue data
-math = 15000-df_clean_country.count()
-print(f"Rougue Country and City rows: {math}\n")
+# Clean up Ecommerce website
+df = df.withColumn(
+    "ecommerce_website_name",
+    trim(col("ecommerce_website_name"))  # remove leading/trailing spaces
+)
+
+# remove literal '\t' characters that appear as text or tabs
+df = df.withColumn(
+    "ecommerce_website_name",
+    when(col("ecommerce_website_name").isNull(), None)
+    .otherwise(trim(regexp_replace(col("ecommerce_website_name"), r"\\t|\t", "")))
+)
+
 #------------------------------------------------------------------------------------------------------------------------------------------------
 # Kevin's clean up code
 
 # Quantity, product_id clean up
-p2_filtered = df.filter(
+df = df.filter(
     col("datetime").isNotNull() &
     col("qty").isNotNull() & (col("qty") > 0) &
     col("product_id").isNotNull()
-    )
+)
+
 # Drop duplicates
-p2_clean = p2_filtered.dropDuplicates()
-
-# Calculate Rogue data
-math2 = (15000 - p2_clean.count()) 
-print(f"Rogue quantity and product_id rows: {math2}\n")
+df = df.dropDuplicates()
 #------------------------------------------------------------------------------------------------------------------------------------------------
-# Calculate the rogue data
-print("Calculations of Rogue data")
-print(f"Total %: (({math} + {math2})/15000) * 100 =")
-print(((math + math2)/15000)*100)
+# Xiuzhen's clean up code
+# 1 === Check if order_id matches exactly one payment_txn_id ===
+order_check = (
+    df.groupBy("order_id")
+      .agg(countDistinct("payment_txn_id").alias("unique_txn_ids"))
+      .filter("unique_txn_ids > 1")
+)
 
+df = df.join(order_check, on="order_id", how="left_anti")
+
+# 2 === Check if product_id maps to exactly one product_name ===
+multi_name_ids = (
+    df.groupBy("product_id")
+      .agg(countDistinct("product_name").alias("unique_names"))
+      .filter(col("unique_names") > 1)
+)
+
+(
+    df.join(multi_name_ids, on="product_id", how="inner")
+      .groupBy("product_id", "product_name")
+      .count()
+      .orderBy("product_id", "count", ascending=False)
+)
+
+df = (
+    df.withColumn(
+        "product_name",
+        # Step 1. Replace tabs, newlines, and multiple spaces with a single space
+        regexp_replace(col("product_name"), r"[\t\n\r]+", " ")
+    )
+    .withColumn(
+        "product_name",
+        regexp_replace(col("product_name"), r"\s{2,}", " ")  # collapse multiple spaces
+    )
+    .withColumn(
+        "product_name",
+        trim(col("product_name"))  # trim leading and trailing spaces
+    )
+    .withColumn(
+        # Step 2. Replace null, empty string, or literal 'NULL' with None
+        "product_name",
+        when(
+            (col("product_name").isNull()) |
+            (col("product_name") == "") |
+            (col("product_name").rlike("(?i)^null$")),
+            None
+        ).otherwise(col("product_name"))
+    )
+)
+multi_name_ids = (
+    df.groupBy("product_id")
+      .agg(countDistinct("product_name").alias("unique_names"))
+      .filter(col("unique_names") > 1)
+)
+
+(
+    df.join(multi_name_ids, on="product_id", how="inner")
+      .groupBy("product_id", "product_name")
+      .count()
+      .orderBy("product_id", "count", ascending=False)
+)
+
+df = df.filter((col("product_name").isNotNull()) & (length(trim(col("product_name"))) > 0))
+
+df = df.join(multi_name_ids, on="product_id", how="left_anti")
+
+# 3 === Clean pattern and value checks ===
+
+cols_to_count = ["product_category","payment_txn_success","qty", "price", "datetime"]
+
+# for payment_txn_success, it has only"F"&"Y"
+df = df.withColumn(
+    "payment_txn_success",
+    regexp_replace(col("payment_txn_success"), "^F$", "N")
+)
+
+before = df.count()
+df_clean = df.filter(
+    (col("order_id").isNotNull()) & (col("order_id").rlike("^oid_[1-9][0-9]*$")) &
+    (col("product_id").isNotNull()) & (col("product_id").rlike("^pid_[1-9][0-9]*$")) &
+    (col("product_category").isNotNull())&
+    (col("qty").isNotNull()) & (col("qty") > 0) &
+    (col("price").isNotNull()) & (col("price") > 0) &
+    (col("datetime").isNotNull()) &
+    (col("payment_txn_success").isin("Y","N"))
+)
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
 # Combine the clean data
 
-# Final column order: 
-final_columns = [
+# Define final schema order
+df_final = df_clean.select(
     "order_id", "customer_id", "customer_name", "product_id",
     "product_name", "product_category", "payment_type", "qty",
     "price", "datetime", "country", "city",
     "ecommerce_website_name", "payment_txn_id",
     "payment_txn_success", "failure_reason"
-]
+)
 
 # Save to CSV file:
-output_path = f"{dir_path}/final_combined.csv"
+output_path = f"{dir_path}/clean_data_final"
 
-# print("Combined CSV created")
-#-------------------------------------------------------------------------------------------------------------------------------------------------
-print("End of Session")
+df_final.coalesce(1).write \
+    .option("header", "true") \
+    .mode("overwrite") \
+    .csv(output_path)
+
 # Stop the Spark session
 spark.stop()
--- Analysis/pyspark_Analysis.py
+++ Analysis/pyspark_Analysis.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, trim, upper, regexp_replace
+from pyspark.sql.functions import col, trim, upper, regexp_replace, when
 
 # Create spark session
 spark = SparkSession.builder \
@@ -32,8 +32,8 @@ df = (
       .withColumn("city", upper(trim(col("city"))))
 )
 
-# Drop rows where city or country are null
-df = df.dropna(subset=["city", "country"])
+# Drop rows where city or country are null plus eccomerce website
+df = df.dropna(subset=["city", "country", "ecommerce_website_name"])
 
 # Remove tabs, newlines, and extra spaces
 df = df.withColumn("city", regexp_replace("city", r"[\t\n\r]+", " "))
@@ -47,17 +47,34 @@ countries.show(100, truncate=False)
 # Incase misspelling happens
 valid_countries = ["CANADA", "FRANCE", "GERMANY", "GREAT BRITAIN", "MEXICO", "UNITED STATES"]
 df_clean_country = df.filter(col("country").isin(valid_countries))
-print("Original rows: ", dfo.count())
-print("Rows before:", df.count())
-print("Rows after:", df_clean_country.count())
+# print("Original rows: ", dfo.count())
+# print("Rows before:", df.count())
+# print("Rows after:", df_clean_country.count())
 
-for c in valid_countries:
-    print(f"\n--- {c} ---")
-    df_clean_country.filter(col("country") == c) \
-        .select("city") \
-        .distinct() \
-        .orderBy("city") \
-        .show(100, truncate=False)
+# for c in valid_countries:
+#     print(f"\n--- {c} ---")
+#     df_clean_country.filter(col("country") == c) \
+#         .select("city") \
+#         .distinct() \
+#         .orderBy("city") \
+#         .show(100, truncate=False)
+
+df.select("ecommerce_website_name").distinct().show(truncate=False)
+
+df_cleaned = df.withColumn(
+    "ecommerce_website_name",
+    trim(col("ecommerce_website_name"))  # remove leading/trailing spaces
+)
+
+# remove literal '\t' characters that appear as text or tabs
+df_cleaned = df_cleaned.withColumn(
+    "ecommerce_website_name",
+    when(col("ecommerce_website_name").isNull(), None)
+    .otherwise(trim(regexp_replace(col("ecommerce_website_name"), r"\\t|\t", "")))
+)
+df_cleaned.select("ecommerce_website_name").distinct().show()
+
+
 
 # Stop the Spark session
 spark.stop()
